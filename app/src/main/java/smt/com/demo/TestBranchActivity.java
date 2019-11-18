69e5539176f2f4a80aa303fa18ac87513a6d1eac
lepaitianshi <yutao_86@yeah.net>
2019-11-18 20:22:18 +0800

    add test branch activity

---
+++ app/src/main/java/smt/com/demo/TestBranchActivity.java
@@ -0,0 +1,13 @@
+package smt.com.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestBranchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_branch);
+    }
+}
